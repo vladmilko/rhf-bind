3b0d2b713e3a86bff38ee16474fa1e903bd28f38
vladmilko <vladmilko27@gmail.com>
2024-10-25 23:36:11 +0300

    Add vite and jest config fabrics

---
+++ configs/vite/createViteConfig.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+import { AliasOptions, defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+interface CreateBuildConfigProps {
+  packageName: string;
+  externalPackages: Record<string, string>;
+  alias?: AliasOptions | undefined;
+  rootDir: string;
+}
+
+export const createBuildConfig = ({ externalPackages, packageName, alias, rootDir }: CreateBuildConfigProps) =>
+  defineConfig({
+    plugins: [dts({ tsconfigPath: path.resolve(rootDir, './tsconfig.json'), rollupTypes: true })],
+    resolve: { alias },
+    build: {
+      outDir: path.resolve(rootDir, 'lib'),
+      lib: {
+        entry: path.resolve(rootDir, 'src/index.ts'),
+        name: packageName,
+        fileName: 'index',
+        formats: ['cjs', 'es', 'umd'],
+      },
+      rollupOptions: {
+        external: Array.from(Object.keys(externalPackages)),
+      },
+      sourcemap: true,
+    },
+  });
